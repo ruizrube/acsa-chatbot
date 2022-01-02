08a966f31c0f170325423d2498e877660a79468f
Iván Ruiz (UCA) <ivanruizrube@192.168.1.132>
2022-01-02 19:25:39 +0100

    Autenticación entre DialogFlow y Java Server

---
+++ botmanager/src/main/java/es/ja/csalud/sas/botcitas/botmanager/SecurityConfig.java
@@ -0,0 +1,40 @@
+package es.ja.csalud.sas.botcitas.botmanager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+	      .csrf().disable().authorizeRequests().antMatchers("/Appointments","/Users","/Clinics","/dialogflow").authenticated().anyRequest().permitAll().and()
+				.httpBasic();
+
+	}
+
+	
+}
